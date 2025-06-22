8a3343049ea98887133a629515d0dd93e53ef50f
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-06-22 23:35:26 +0530

    Update game-controller.ts to handle new game state

--- game-controller.ts
+++ game-controller.ts
@@ -10,10 +10,42 @@ import {
 import Coup from "./src/models/coup.ts";
 import Player from "./src/models/player.ts";
 
+const makePlayerChoice = (player: Player): number => {
+  const choice = prompt(`It's your turn, ${player.name}. \nChoose an action:
+    1. Income
+    2. Foreign Aid
+    3. Coup
+    4. Character Action
+  Enter the number of your choice: `)?.trim();
+
+  return parseInt(choice ?? "") ?? makePlayerChoice(player);
+};
+
+const playAction = (game: Coup, choice: number) => {
+  switch (choice) {
+    case 1:
+      return game.income();
+
+    case 2:
+      return game.foreignAid();
+
+    case 3:
+      return game.coup(targetPlayerId);
+
+    case 4:
+      return game.characterAction(character);
+  }
+};
+
 const playGameRound = (game: Coup) => {
   const currentPlayer = game.currentPlayer();
 
   console.log(`Current Player: ${currentPlayer.name}`);
+  const choice = makePlayerChoice(currentPlayer);
+
+  console.log(`Player ${currentPlayer.name} chose: ${choice}`);
+
+  playAction(game, choice);
 };
 
 const gameCards = () => [
